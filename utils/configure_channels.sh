93fc34373cd9d59c148dbb99cafefe5f27dba7ed
JasonPowr <japower@redhat.com>
2025-09-10 15:52:29 +0100

    ci: update channels script

--- utils/configure_channels.sh
+++ utils/configure_channels.sh
@@ -16,17 +16,23 @@ for channel in "${channel_list[@]}"; do
     echo "Channel $channel exists. Updating..."
     
     CURRENT_ENTRIES=$(yq e ".entries[] | select(.schema==\"olm.channel\" and .name==\"$channel\").entries[].name" $GRAPH || true)
-    FILTERED_ENTRIES=$(echo "$CURRENT_ENTRIES" | grep -v "^${BUNDLE_NAME}$" || true)
+    ENTRIES_WO_BUNDLE=$(echo "$CURRENT_ENTRIES" | grep -v "^${BUNDLE_NAME}$" || true)
     
-    if [[ -n "$FILTERED_ENTRIES" ]]; then
-      export OLDEST=$(echo "$FILTERED_ENTRIES" | sort -V | head -n 1)
-      export SKIPS=$(echo "$FILTERED_ENTRIES" | awk '{print "\"" $0 "\""}' | paste -sd, - | sed 's/^/[/' | sed 's/$/]/')
-      
+    if [[ -n "$ENTRIES_WO_BUNDLE" ]]; then
+      export MAJOR_VERSIONS=$(echo $ENTRIES_WO_BUNDLE | tr ' ' '\n' | grep -E '\.0$')
+      export REPLACES=$(echo "$MAJOR_VERSIONS" | sort -V | tail -n 1)
+      export SKIPS=$(echo "$ENTRIES_WO_BUNDLE" \
+        | grep -v -E '\.v1\.(0|1)\.[0-9]+' \
+        | awk '{print "\"" $0 "\""}' \
+        | paste -sd, - \
+        | sed 's/^/[/' | sed 's/$/]/'
+      )
+
       yq -i -P eval "
         (.entries[] | select(.schema == \"olm.channel\" and .name == \"$channel\").entries) |=
           (map(select(.name != env(BUNDLE_NAME))) + [{
             \"name\": env(BUNDLE_NAME),
-            \"replaces\": env(OLDEST),
+            \"replaces\": env(REPLACES),
             \"skips\": (strenv(SKIPS) | fromjson)
           }])
       " $GRAPH
